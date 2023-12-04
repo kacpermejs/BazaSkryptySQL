17637bc4aa6d889f7340dbbe5c5493d1668ccf53
Kamil-Lontkowski <kamillont14@gmail.com>
2023-12-04 19:11:39 +0100

    Data rozpoczecia index

--- idx1.sql
+++ idx1.sql
@@ -6,6 +6,8 @@ DROP INDEX idx_id_recepty_lrs;
 
 --Select 2 - wizyty funkcyjny
 
+CREATE INDEX idx_data_rozpoczecia ON WIZYTA (EXTRACT YEAR FROM(ROZPOCZECIE));
+
 --Select 3 - klucz obcy id_lekarstwa w lekarstwo_recepta
 CREATE INDEX idx_id_lekarstwa_lr ON lekarstwo_recepty (id_lekarstwa);
 
