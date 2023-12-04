579da8decc22a9f582854ef04bfc1a9f5a9f4351
Kacper Mejsner <kacpermejs@gmail.com>
2023-12-04 18:17:32 +0100

    lr

--- idx1.sql
+++ idx1.sql
@@ -1,8 +1,15 @@
 --Select 1 - klucz obcy id_recepty w lekarstwo_recepta
+CREATE INDEX idx_id_recepty_lr ON lekarstwo_recepta (id_recepty);
+
+DROP INDEX idx_id_recepty_lr ON lekarstwo_recepta;
+
 
 --Select 2 - wizyty funkcyjny
 
 --Select 3 - klucz obcy id_lekarstwa w lekarstwo_recepta
+CREATE INDEX idx_id_lekarstwa_lr ON lekarstwo_recepta (id_lekarstwa);
+
+DROP INDEX idx_id_lekarstwa_lr ON lekarstwo_recepta;
 
 --Select 4 pomysł Kamila
     
