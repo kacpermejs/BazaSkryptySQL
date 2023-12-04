bf292f74f23ada63dd0cd66e2ae994d47755b38a
Kamil-Lontkowski <kamillont14@gmail.com>
2023-12-04 20:44:51 +0100

    Drugi indeks

--- idx1.sql
+++ idx1.sql
@@ -1,19 +1,14 @@
 --Select 1 - klucz obcy id_recepty w lekarstwo_recepta
-CREATE INDEX idx_id_recepty_lr ON lekarstwo_recepty (id_recepty);
-
-DROP INDEX idx_id_recepty_lrs;
-
 
 --Select 2 - wizyty funkcyjny
 
 CREATE INDEX idx_data_rozpoczecia ON WIZYTA (EXTRACT(YEAR FROM(ROZPOCZECIE)));
 
 --Select 3 - klucz obcy id_lekarstwa w lekarstwo_recepta
-CREATE INDEX idx_id_lekarstwa_lr ON lekarstwo_recepty (id_lekarstwa);
 
-DROP INDEX idx_id_lekarstwa_lr;
+CREATE INDEX idx_wiz ON WIZYTA (STATUS, CZY_PRYWATNA, REFUNDACJA);
 
 --Select 4 pomysł Kamila
     
-    
+-- Tu będzie też funkcyjny jak w select 2 i range jak na podstawie interwału    
     
---
+++ zmienione_kwery/SELECT_3.sql
@@ -0,0 +1,18 @@
+SELECT COUNT(*) 
+            FROM PACJENT p
+            JOIN OSOBA o ON o.id = p.id_osoby
+            JOIN PRZYCHODNIA prz ON p.id_przychodni = prz.id
+            WHERE p.rodzaj_swiadczenia = 'Premium+' AND
+            p.id IN (
+              SELECT DISTINCT w.id_pacjenta
+              FROM WIZYTA w
+              JOIN (
+                SELECT r.id_pacjenta AS pacjent_ID
+                FROM RECEPTY r
+                JOIN LEKARSTWO_RECEPTY lr ON lr.id_recepty = r.id
+                JOIN LEKARSTWO l ON l.id = lr.id_lekarstwa
+                GROUP BY r.id_pacjenta
+                HAVING COUNT(DISTINCT lr.id_lekarstwa) >= 3 AND COUNT(DISTINCT l.nazwa_producenta) >= 3
+              ) ON w.id_pacjenta = pacjent_ID
+              WHERE w.STATUS = 'zakonczona' AND w.CZY_PRYWATNA = 'T' AND w.REFUNDACJA IS NULL
+            );
